bd258f4742ba324942d3af63c573609c7b59a113
WizziGameDev <rickyadamsaputra1.com>
2025-04-20 15:34:53 +0700

    Update: Clean not usage code

--- src/main/java/com/service/patient_service/service/PatientService.java
+++ src/main/java/com/service/patient_service/service/PatientService.java
@@ -8,17 +8,17 @@ import java.util.List;
 public interface PatientService {
 
     // Get All Patient Not Deleted
-    public List<PatientResponse> getPatients();
+    List<PatientResponse> getPatients();
 
     // Get Patient By slug
-    public PatientResponse getPatientBySlug(String slug);
+    PatientResponse getPatientBySlug(String slug);
 
     // Add Patient
-    public PatientResponse addPatient(PatientRequest patientRequest);
+    PatientResponse addPatient(PatientRequest patientRequest);
 
     // Update Patient
-    public PatientResponse updatePatient(PatientRequest patientRequest, String slug);
+    PatientResponse updatePatient(PatientRequest patientRequest, String slug);
 
     // Delete Patient
-    public String deletePatient(String slug);
+    String deletePatient(String slug);
 }
